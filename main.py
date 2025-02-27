bace1c95049d13f0e43512ca7a95b762edf5d78d
Sirawit Loywirat <sloywirat@gmail.com>
2025-02-27 17:51:40 +0700

    Add next tetromino preview

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from kivy.uix.label import Label
 from kivy.core.text import LabelBase
 from kivy.clock import Clock
 from kivy.core.window import Window
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, StringProperty
 from tetris import Tetris
 
 LabelBase.register(name="Jersey10", fn_regular="./font/Jersey10-Regular.ttf")
@@ -29,6 +29,16 @@ TETROMINO_COLORS = {
     9: [0, 0, 0],
 }
 
+TETROMINO_SHAPES = {
+    "T": [[0, 1, 0, 0], [1, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+    "I": [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]],
+    "O": [[0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+    "L": [[0, 0, 1, 0], [1, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+    "J": [[1, 0, 0, 0], [1, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+    "S": [[0, 1, 1, 0], [1, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+    "Z": [[1, 1, 0, 0], [0, 1, 1, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+}
+
 Window.clearcolor = (0.1, 0.1, 0.1, 1)
 
 
@@ -36,6 +46,7 @@ class TetrisBoard(Widget):
     lines_cleared = NumericProperty(0)
     level = NumericProperty(0)
     score = NumericProperty(0)
+    next_tetromino = StringProperty("")
 
     def __init__(self, init_pos, **kwargs) -> None:
         super().__init__(**kwargs)
@@ -53,6 +64,7 @@ class TetrisBoard(Widget):
         self.total_clear_line = self.game.total_clear_line
         self.level = self.game.level
         self.score = self.game.score
+        self.next_tetromino = self.game.bag.next_piece
         self.blocks = [[None] * GRID_COLS for _ in range(GRID_ROWS)]
 
         self._keyboard = Window.request_keyboard(self._on_keyboard_closed, self)
@@ -152,6 +164,7 @@ class TetrisBoard(Widget):
         cleared_lines = self.game.total_clear_line
         level = self.game.level
         score = self.game.score
+        next_tetromino = self.game.bag.next_piece
 
         if cleared_lines != self.lines_cleared:
             self.lines_cleared = cleared_lines
@@ -159,6 +172,8 @@ class TetrisBoard(Widget):
             self.level = level
         if score != self.score:
             self.score = score
+        if next_tetromino != self.next_tetromino:
+            self.next_tetromino = next_tetromino
 
     def _on_keyboard_closed(self) -> None:
         self._keyboard.unbind(on_key_down=self._on_key_down)
@@ -191,6 +206,36 @@ class TetrisBoard(Widget):
             exit()
 
 
+class NextTetrominoWidget(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.size_hint = (None, None)
+        self.size = (4 * CELL_SIZE // 1.2, 4 * CELL_SIZE // 1.2)
+        self.canvas.clear()
+        self.blocks = []
+
+    def update_tetromino(self, tetromino: str) -> None:
+        piece_shape = TETROMINO_SHAPES[tetromino]
+        self.canvas.clear()
+        self.blocks.clear()
+
+        with self.canvas:
+            Color(1, 1, 1, 1)
+            for row in range(4):
+                for col in range(4):
+                    if piece_shape[row][col]:
+                        rect = Rectangle(
+                            pos=(
+                                (self.x - CELL_SIZE // 2) + col * (CELL_SIZE // 1.2),
+                                (self.y + CELL_SIZE // 2)
+                                + (3 - row) * (CELL_SIZE // 1.2),
+                            ),
+                            size=(CELL_SIZE // 1.2, CELL_SIZE // 1.2),
+                        )
+                        self.blocks.append(rect)
+                        self.canvas.add(rect)
+
+
 class TetrisApp(App):
     def create_label_pair(self, text: str, value: str) -> tuple[BoxLayout, Label]:
         container = BoxLayout(
@@ -218,6 +263,8 @@ class TetrisApp(App):
         )
 
         self.tetris_board = TetrisBoard(init_pos)
+        self.next_tetromino_widget = NextTetrominoWidget()
+
         self.info_panel = BoxLayout(
             orientation="vertical",
             spacing=120,
@@ -233,6 +280,17 @@ class TetrisApp(App):
         self.level_container, self.level_value = self.create_label_pair("LEVEL", "1")
         self.score_container, self.score_value = self.create_label_pair("SCORE", "0")
 
+        next_tetromino_label_container = BoxLayout(
+            orientation="vertical", size_hint=(None, None), spacing=64
+        )
+        next_tetromino_label = Label(
+            text="NEXT",
+            font_size=72,
+            font_name="Jersey10",
+        )
+        next_tetromino_label_container.add_widget(next_tetromino_label)
+        self.info_panel.add_widget(next_tetromino_label_container)
+        self.info_panel.add_widget(self.next_tetromino_widget)
         self.info_panel.add_widget(self.lines_container)
         self.info_panel.add_widget(self.level_container)
         self.info_panel.add_widget(self.score_container)
@@ -240,6 +298,7 @@ class TetrisApp(App):
         self.tetris_board.bind(lines_cleared=self.update_lines)
         self.tetris_board.bind(level=self.update_levels)
         self.tetris_board.bind(score=self.update_scores)
+        self.tetris_board.bind(next_tetromino=self.update_next_tetromino)
 
         root = FloatLayout()
         root.add_widget(self.tetris_board)
@@ -255,6 +314,9 @@ class TetrisApp(App):
 
     def update_scores(self, instance, value):
         self.score_value.text = str(value)
+
+    def update_next_tetromino(self, instance, value):
+        self.next_tetromino_widget.update_tetromino(value)
 
 
 if __name__ == "__main__":
