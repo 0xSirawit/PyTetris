f09910d0bc9487ac80863dd4946993b21c944c6e
pechchanan <91284775+tawanza11@users.noreply.github.com>
2025-03-01 01:46:24 +0700

    Add TetrisApp into game menu

--- menu.py
+++ menu.py
@@ -4,9 +4,14 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import Screen, ScreenManager
-import pygame
+from kivy.core.window import Window
+from main import *
 
-# เมนูเกม
+SCREEN_RESOLUTION = (1920, 1080)
+GRID_COLS, GRID_ROWS = 10, 20
+
+
+# Game_menu
 class MenuScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -41,7 +46,7 @@ class MenuScreen(Screen):
 
         btn_start.bind(on_press=self.start_game)
         btn_high_scores.bind(on_press=self.show_high_scores)
-        btn_exit.bind(on_press=lambda : App.get_running_app().stop())
+        btn_exit.bind(on_press=lambda: App.get_running_app().stop())
 
         layout.add_widget(title)
         layout.add_widget(btn_start)
@@ -50,14 +55,16 @@ class MenuScreen(Screen):
         self.add_widget(layout)
 
     def start_game(self, instance):
-        self.manager.current = 'game_screen'  # เปลี่ยนไปหน้าจอเกม
+        self.manager.current = "game_screen"
 
     def show_high_scores(self, instance):
         content = BoxLayout(orientation="vertical")
-        content.add_widget(Label(
-            text="High Scores:\n1. Player1 - 10000\n2. Player2 - 8000\n3. Player3 - 6000",
-            font_size=24
-        ))
+        content.add_widget(
+            Label(
+                text="High Scores:\n1. Player1 - 10000\n2. Player2 - 8000\n3. Player3 - 6000",
+                font_size=24,
+            )
+        )
         close_btn = Button(text="Close", size_hint=(1, 0.2))
         content.add_widget(close_btn)
 
@@ -65,21 +72,97 @@ class MenuScreen(Screen):
         close_btn.bind(on_press=popup.dismiss)
         popup.open()
 
-# หน้าจอเกม
+
+# Game_screen
 class GameScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        layout = BoxLayout(orientation='vertical')
-        layout.add_widget(Label(text='Game is running!', font_size=24))
-        self.add_widget(layout)
 
-# แอปหลัก
+        init_pos = (
+            (((SCREEN_RESOLUTION[0] - (GRID_COLS * CELL_SIZE)) / 2) - 120),
+            (((SCREEN_RESOLUTION[1] - (GRID_ROWS * CELL_SIZE)) / 2)),
+        )
+
+        self.tetris_board = TetrisBoard(init_pos)
+        self.next_tetromino_widget = NextTetrominoWidget()
+
+        self.info_panel = BoxLayout(
+            orientation="vertical",
+            spacing=120,
+            size_hint=(None, None),
+            size=(400, 300),
+            pos=(
+                self.tetris_board.width + init_pos[0] + 120,
+                init_pos[1] + 100,
+            ),
+        )
+
+        self.lines_container, self.lines_value = self.create_label_pair("LINES", "0")
+        self.level_container, self.level_value = self.create_label_pair("LEVEL", "1")
+        self.score_container, self.score_value = self.create_label_pair("SCORE", "0")
+
+        next_tetromino_label_container = BoxLayout(
+            orientation="vertical", size_hint=(None, None), spacing=64
+        )
+        next_tetromino_label = Label(
+            text="NEXT",
+            font_size=72,
+            font_name="Jersey10",
+        )
+        next_tetromino_label_container.add_widget(next_tetromino_label)
+        self.info_panel.add_widget(next_tetromino_label_container)
+        self.info_panel.add_widget(self.next_tetromino_widget)
+        self.info_panel.add_widget(self.lines_container)
+        self.info_panel.add_widget(self.level_container)
+        self.info_panel.add_widget(self.score_container)
+
+        self.tetris_board.bind(lines_cleared=self.update_lines)
+        self.tetris_board.bind(level=self.update_levels)
+        self.tetris_board.bind(score=self.update_scores)
+        self.tetris_board.bind(next_tetromino=self.update_next_tetromino)
+
+        self.add_widget(self.tetris_board)
+        self.add_widget(self.info_panel)
+
+        Clock.schedule_once(
+            lambda dt: self.next_tetromino_widget.update_tetromino(
+                self.tetris_board.game.bag.next_piece
+            )
+        )
+
+    def create_label_pair(self, text: str, value: str) -> tuple:
+        container = BoxLayout(
+            orientation="vertical", size_hint=(None, None), spacing=64
+        )
+        label = Label(text=text, font_size=72, font_name="Jersey10")
+        value_label = Label(text=value, font_size=64, font_name="Jersey10")
+        container.add_widget(label)
+        container.add_widget(value_label)
+        return container, value_label
+
+    def update_lines(self, instance, value):
+        self.lines_value.text = str(value)
+
+    def update_levels(self, instance, value):
+        self.level_value.text = str(value)
+
+    def update_scores(self, instance, value):
+        self.score_value.text = str(value)
+
+    def update_next_tetromino(self, instance, value):
+        self.next_tetromino_widget.update_tetromino(value)
+
+
+# Main_app
 class GameApp(App):
     def build(self):
         sm = ScreenManager()
-        sm.add_widget(MenuScreen(name='menu_screen'))
-        sm.add_widget(GameScreen(name='game_screen'))
+        Window.clearcolor = (0.1, 0.1, 0.1, 1)
+        Window.fullscreen = "auto"
+        sm.add_widget(MenuScreen(name="menu_screen"))
+        sm.add_widget(GameScreen(name="game_screen"))
         return sm
+
 
 if __name__ == "__main__":
     app = GameApp()
